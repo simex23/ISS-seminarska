50e185c0cdc85178d3934960206203a9e59d853e
simex23 <jansimon.aqua@gmail.com>
2017-04-14 14:53:29 +0200

    added for required fields in register

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 function verify(){
 	var error=0;
 
-	//verify email
+//verify email
 	var r=document.register.elements[2];
 	var atpos=r.value.indexOf("@");
 	var dotpos=r.value.lastIndexOf(".");
-	console.log(atpos,dotpos);
+	//console.log(atpos,dotpos);
 	if(dotpos<atpos || r.value==""){
 		r.style.borderColor="#ff0000";
-		alert("Not a valid email address");
+		alert("Not a valid email address!");
 		if(error==0)
 			r.focus();
 		error++;
 	}
 
-	//password + retype password match
+//verify username
+	var r=document.register.elements[3];
+	if(r.value==""){
+		r.style.borderColor="#ff0000";
+		alert("Please specify the username!");
+		if(error==0)
+			r.focus();
+		error++;
+	}
+
+//password + retype password match
 	var r=document.register.elements[4];
 	var r2=document.register.elements[5];
 	if(r.value!=r2.value){
